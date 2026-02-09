c0486f405036e8201258b73815d151c3c093fe44
fortune <fortunerohi27@gmail.com>
2026-02-09 11:13:47 +0100

    first commit

---
+++ src/one.java
@@ -0,0 +1,8 @@
+public class one {
+    static void main(String[] args) {
+        int itemPrice = 50;
+        int shoppingCost = 90;
+        int sum = itemPrice + shoppingCost;
+        System.out.println(sum);
+    }
+}
