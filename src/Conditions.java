13df5466c5573f993a555617a9c4b08618330081
fortune <fortunerohi27@gmail.com>
2026-02-18 11:09:37 +0100

    second commit

---
+++ src/Conditions.java
@@ -0,0 +1,12 @@
+public class Conditions {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age >= 18){
+            System.out.println("Allowed");
+        }  else{
+                System.out.println("Not Allowed");
+            }
+
+        }
+}
---
+++ src/Rage.java
@@ -0,0 +1,10 @@
+public class Rage {
+    public static void WelcomeMessage() {
+        System.out.println("Welcome to Java!");
+    }
+    public static void main(String[] args) {
+        WelcomeMessage();
+    }
+
+    
+}
