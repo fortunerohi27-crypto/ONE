bc550a5223393c6f910fab244231ff15b03a1564
fortune <fortunerohi27@gmail.com>
2026-02-23 11:27:31 +0100

    more work

---
+++ src/method.java
@@ -0,0 +1,9 @@
+public class method {
+    String city ="London";
+    int population = 9000000;
+    public static void main(String[] args){
+     method myobj = new method();
+     System.out.println(myobj.city);
+     System.out.println(myobj.population);
+    }
+}
